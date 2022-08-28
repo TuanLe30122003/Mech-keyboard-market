01f4c57aeeb0c13a275bce51f4c8f053d9c81733
TuanLe30122003 <91524625+TuanLe30122003@users.noreply.github.com>
2022-08-28 21:59:06 +0700

    28/8/2022 finish the input css and js, ready for stage 3

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let home = document.querySelector('#home');
-let explore = document.querySelector('#explore')
+const home = document.querySelector('#home');
+const explore = document.querySelector('#explore')
 
 home.onclick = function() {
     // document.getElementById('explorestage').style.display = "none";
@@ -13,6 +13,7 @@ home.onclick = function() {
     }
 
     document.getElementById('homestage').style.display = "block";
+    // document.getElementById('homestage').style.transition ="all 0.5s";
 
 
     var buttonn = document.getElementsByClassName('nav-but');
@@ -27,6 +28,7 @@ home.onclick = function() {
     // console.log(buttonn.length);
 }
 
+
 explore.onclick = function() {
     // document.getElementById('homestage').style.display = "none";
 
@@ -35,7 +37,7 @@ explore.onclick = function() {
 
     for(let i=0; i < stagee.length; i++) {
         stagee[i].style.display = "none";
-        console.log(i);
+        // console.log(i);
     }
 
     document.getElementById('explorestage').style.display = "block";
@@ -49,6 +51,51 @@ explore.onclick = function() {
     document.getElementById('explore').style.color = "#fff";
 }
 
+home.click();
 
+
+const cateIcon = document.querySelector('.cate div ion-icon');
+const availableIcon = document.querySelector('.available div ion-icon');
+const cateChoice = document.querySelector('.cate .choice');
+const availableChoice = document.querySelector('.available .choice');
+// console.log(choice);
+
+cateIcon.onclick = function (e) {
+    cateChoice.classList.toggle('unable');
+}
+
+availableIcon.onclick = function (e) {
+    availableChoice.classList.toggle('unable');
+}
+
+const choosenCate = document.querySelectorAll('.cate li');
+const choosenAvailable = document.querySelectorAll('.available li');
+
+const choiceBox = document.querySelectorAll('.choiceBox');
+// console.log(choiceBox);
+
+for(var i=0; i<choosenCate.length; i++){
+    choosenCate[i].onclick = function (e) {
+        var needToChange = document.querySelector('.cate .choiceBox span');
+        needToChange.innerText = e.target.innerText;
+        cateChoice.classList.toggle('unable');
+        choiceBox[0].style.backgroundColor = "#fff";
+        choiceBox[0].style.color = "#7453fc";
+        choiceBox[0].style.fontWeight = "700";
+        cateIcon.style.color = "#000";
+    }
+}
+
+for(var i=0; i<choosenAvailable.length; i++){
+    choosenAvailable[i].onclick = function (e) {
+        var needToChange = document.querySelector('.available .choiceBox span');
+        needToChange.innerText = e.target.innerText;
+        availableChoice.classList.toggle('unable');
+        choiceBox[1].style.backgroundColor = "#fff";
+        choiceBox[1].style.color = "#7453fc";
+        choiceBox[1].style.fontWeight = "700";
+        availableIcon.style.color = "#000";
+    }
+}
 
 
